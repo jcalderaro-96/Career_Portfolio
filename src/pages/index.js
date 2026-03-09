8cafa3ee48d4946361dfc30259283446921968fd
jcalderaro-96 <jcalderaro96@gmail.com>
2026-03-08 19:52:03 -0700

    Imported Montserrat Font and test verify

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-black text-white">Home</main>
+      <main className="p-10">
+        <h1 className="text-6xl font-mont font-bold">Home</h1>
+        <p className="mt-4 text-xl font-mont">Testing Montserrat font</p>
+      </main>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mont: ["var(--font-mont)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
